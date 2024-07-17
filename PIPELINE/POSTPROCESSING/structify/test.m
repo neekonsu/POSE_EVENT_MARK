ebfd0131a1415611353c8227de1f61ea05f6bf8a
NeuroRestoreFR <neekon.saadat@epfl.ch>
2024-07-17 13:25:46 +0200

    sync commit

--- PIPELINE/POSTPROCESSING/structify/test.m
+++ PIPELINE/POSTPROCESSING/structify/test.m
@@ -1,6 +1,7 @@
 % 
 % Add summary helper-function to path
-addpath("../../PREPROCESSING/mat_struct_summary/")
+addpath("../../PREPROCESSING/mat_struct_summary/");
+addpath("format_evt_struct");
 % Prompt user for input mat file to convert
 [evt_file, path] = uigetfile("*.mat", "Select Event File");
 % Add parent directory to path
@@ -10,7 +11,7 @@ old_struct = load(evt_file);
 % Update old struct with new formatting
 updated_struct = format_evt_struct(old_struct);
 % Save Struct Locally
-save("testNewStructEvt", 'updated_struct');
+save(evt_file, 'updated_struct');
 % Summarize New Struct
 % mat_struct_summary();
 
